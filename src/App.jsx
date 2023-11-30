bafde6e8a328a56b2795e718714002c01e43d46d
01Akshayjangra <akshayjangra225@gmail.com>
2023-11-30 16:26:43 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  ViewProject,
+} from "./components";
+
+const Main = () =>{
+  return (
+    <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+  )
+}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Main/>}/>
+        {/* <Route exact path='/view-project' element={<ViewProject/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/ViewProject.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ViewProject = () => {
+  return (
+    <>
+    <div className="text-white">
+        <h1>cxvvcxvxcv</h1>
+    </div>
+    </>
+  )
+}
+
+export default ViewProject
